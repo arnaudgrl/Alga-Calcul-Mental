80532879fda818e943fe9d8510970fbb9476c43c
Arnaud Gariel <arnaud.gariel@gmail.com>
2023-11-17 14:37:59 +0100

    Working version without any issues
    Created brand new project to fit the version of particular components.

---
+++ Alga_dev/app/src/main/java/com/example/alga_dev/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.alga_dev
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.alga_dev.ui.theme.Alga_devTheme
+import com.example.alga_dev.hellocard.HelloCard
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Alga_devTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //Greeting("Android")
+                    HelloCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Alga_devTheme {
+        HelloCard()
+    }
+}
