6e146c483b1c42f44de82ebef8ea1b2df08a9892
Arnaud Gariel <arnaud.gariel@gmail.com>
2023-11-21 11:44:04 +0100

    playing game is operational

--- Alga_dev/app/build.gradle.kts
+++ Alga_dev/app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
     implementation("androidx.compose.ui:ui-android:1.5.4")
     implementation("androidx.navigation:navigation-compose:2.4.0")
+    implementation("androidx.compose.runtime:runtime:1.0.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Alga_dev/app/src/main/java/com/example/alga_dev/MainActivity.kt
+++ Alga_dev/app/src/main/java/com/example/alga_dev/MainActivity.kt
@@ -7,6 +7,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,8 +21,12 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.alga_dev.gamemenu.GameMenu
 import com.example.alga_dev.mainpage.MainPage
+import com.example.alga_dev.playpage.PlayPage
 import com.example.alga_dev.settingspage.SettingsPage
 import com.example.alga_dev.ui.theme.Alga_devTheme
+import kotlinx.coroutines.delay
+import java.text.DecimalFormat
+import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +47,100 @@ class MainActivity : ComponentActivity() {
                     composable("gameMenu") {
                         AtMenuPage(navController = navController)
                     }
+                    composable("playGame") {
+                        PlayingGamePage(navController = navController)
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun PlayingGamePage(navController: NavController) {
+    var questionState by remember { mutableStateOf(generateRandomQuestion()) }
+    var answersState by remember { mutableStateOf(generateRandomAnswers(questionState.answer)) }
+
+    var score by remember { mutableStateOf(0) }
+    var timer by remember { mutableStateOf(0) }
+
+    // LaunchedEffect to increment timer every second
+    LaunchedEffect(timer) {
+        while (true) {
+            delay(1000)
+            timer++
+        }
+    }
+
+    PlayPage(
+        goToMainFromGame = { navController.navigate("gameMenu") },
+        questionTextContent = questionState.question,
+        param1TextContent = answersState[0],
+        param2TextContent = answersState[1],
+        param3TextContent = answersState[2],
+        param4TextContent = answersState[3],
+        firstPropositionTapped = {
+            if (answersState[0] == questionState.answer.toString()) {
+                score++
+            }
+            questionState = generateRandomQuestion()
+            answersState = generateRandomAnswers(questionState.answer)
+        },
+        secondPropositionTapped = {
+            if (answersState[1] == questionState.answer.toString()) {
+                score++
+            }
+            questionState = generateRandomQuestion()
+            answersState = generateRandomAnswers(questionState.answer)
+        },
+        thirdPropositionTapped = {
+            if (answersState[2] == questionState.answer.toString()) {
+                score++
+            }
+            questionState = generateRandomQuestion()
+            answersState = generateRandomAnswers(questionState.answer)
+        },
+        fourthPropositionTapped = {
+            if (answersState[3] == questionState.answer.toString()) {
+                score++
+            }
+            questionState = generateRandomQuestion()
+            answersState = generateRandomAnswers(questionState.answer)
+        },
+        scoreTextContent = "$score",
+        timeTextContent = "${formatTime(timer)}"
+    )
+}
+data class QuestionState(val question: String, val answer: Int)
+
+// Function to format time in MM:SS format
+fun formatTime(seconds: Int): String {
+    val minutes = seconds / 60
+    val remainingSeconds = seconds % 60
+    val df = DecimalFormat("00")
+    return "${df.format(minutes)}:${df.format(remainingSeconds)}"
+}
+
+fun generateRandomQuestion(): QuestionState {
+    val operand1 = Random.nextInt(1, 21)
+    val operand2 = Random.nextInt(1, 21)
+    val question = "$operand1 x $operand2"
+    val answer = operand1 * operand2
+    return QuestionState(question, answer)
+}
+
+fun generateRandomAnswers(correctAnswer: Int): List<String> {
+    val incorrectAnswers = mutableListOf<Int>()
+    while (incorrectAnswers.size < 3) {
+        val randomIncorrectAnswer = Random.nextInt(1, 401)
+        if (randomIncorrectAnswer != correctAnswer && !incorrectAnswers.contains(randomIncorrectAnswer)) {
+            incorrectAnswers.add(randomIncorrectAnswer)
+        }
+    }
+    val answers = mutableListOf<String>()
+    answers.add(correctAnswer.toString())
+    answers.addAll(incorrectAnswers.map { it.toString() })
+    return answers.shuffled()
 }
 
 @Composable
@@ -56,11 +155,11 @@ fun AtMainPage(navController: NavController) {
 fun AtMenuPage(navController: NavController) {
     GameMenu(
         goToMainFromMenu = { navController.navigate("mainPage") },
+        goToAdd = { navController.navigate("playGame") },
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Blue)
     )
-
 }
 
 @Composable
@@ -79,12 +178,3 @@ fun AtSettingsPage(navController: NavController) {
         )
     }
 }
-
-
-
-
-
-
-
-
-
